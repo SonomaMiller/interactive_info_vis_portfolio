df66e531a28fb2e9dd86129975b8a9c108db238d
sowassup <sowassup@gmail.com>
2026-02-02 15:17:07 -0800

    updating sketch names and id's for consistency, starting on first p5.js sketch

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -1,14 +1,42 @@
 // Instance-mode sketch for tab 2: spiral clock
-registerSketch('sk2', function (p) {
+registerSketch('spiralclock', function (p) {
+// const spiralclock = (p) => {
+  let angle = 0; // starting angle
+  let radius = 0; // radius of the circle motion
+  let spd = 0.005; // speed of the motion
+  let radiusUp = 0.05; //increase the radius
+
+  let previousX; // declared previous x position variable
+  let previousY; // declared previous y position variable
+
   p.setup = function () {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+    createCanvas(400, 400);
+    background(255);
+    rectMode(CENTER);
   };
+
   p.draw = function () {
-    p.background(220);
-    p.fill(100, 150, 240);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. A', p.width / 2, p.height / 2);
+    // calculate the x and y coordinates of the circle
+    let x = width / 2 + cos(angle) * radius;
+    let y = height / 2 + sin(angle) * radius;
+
+    // increase the angle to create motion
+    angle += spd;
+
+    // increase the radius to make it grow as spiral
+    radius += radiusUp;
+
+    // draw the circle
+    stroke(0);
+    strokeWeight(2);
+    line(x, y, previousX, previousY); //throws a wild error in the first loop, but work from the second loop :D
+
+    // save current position as previous position
+    previousX = x;
+    previousY = y;
   };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
+
+  p.windowResized = function() { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 });
+
+// let myp5 = new p5(spiralclock, 'sk2');
