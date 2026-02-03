d088f05de567b9e537906aeb040f30fd913e4b6d
sowassup <sowassup@gmail.com>
2026-02-02 22:19:10 -0800

    add bomb drawing

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -1,5 +1,6 @@
 // Instance-mode sketch for tab 4: bomb timer
 registerSketch('sk4', function (p) {
+
   let minutesInput, secondsInput;
   let startButton, pauseButton, restartButton;
 
@@ -9,32 +10,32 @@ registerSketch('sk4', function (p) {
   let lastMillis = 0;
 
   p.setup = function () {
-    p.createCanvas(400, 200);
+    p.createCanvas(400, 400);
     p.textAlign(p.CENTER, p.CENTER);
     p.textSize(32);
 
     // user imput fields
     minutesInput = p.createInput('');
     minutesInput.attribute('placeholder', 'minutes');
-    minutesInput.position(50, 150);
+    minutesInput.position(100, 300);
     minutesInput.size(50);
 
     secondsInput = p.createInput('0');
     secondsInput.attribute('placeholder', 'seconds');
-    secondsInput.position(150, 150);
+    secondsInput.position(250, 300);
     secondsInput.size(50);
 
     // Buttons :3
     startButton = p.createButton('Start');
-    startButton.position(250, 140);
+    startButton.position(100, 340);
     startButton.mousePressed(startTimer);
 
     pauseButton = p.createButton('Pause');
-    pauseButton.position(250, 170);
+    pauseButton.position(175, 340);
     pauseButton.mousePressed(pauseTimer);
 
     restartButton = p.createButton('Restart');
-    restartButton.position(320, 140);
+    restartButton.position(250, 340);
     restartButton.mousePressed(restartTimer);
   };
 
@@ -61,6 +62,30 @@ registerSketch('sk4', function (p) {
     remainingSeconds = totalSeconds;
   }
 
+  function drawBomb(x, y, size) {
+    p.push();
+    p.translate(x, y);
+
+    // bomb body
+    p.noStroke();
+    p.fill(60); // dark grey
+    p.circle(0, 0, size);
+
+    // cap thing
+    let capWidth = size * 0.35;
+    let capHeight = size * 0.2;
+    p.rectMode(p.CENTER);
+    p.rect(0, -size / 2 - capHeight / 2, capWidth, capHeight, 4);
+
+    // Wick
+    p.fill("tan");
+    let wickWidth = size * 0.05;
+    let wickHeight = size * 0.25;
+    p.rect(0, -size / 2 - capHeight - wickHeight / 2, wickWidth, wickHeight);
+
+    p.pop();
+  }
+
   p.draw = function () {
     p.background(240);
 
@@ -75,11 +100,15 @@ registerSketch('sk4', function (p) {
       }
     }
 
-    // display
+    let bombSize = 150;
+    drawBomb(p.width / 2, p.height / 2, bombSize);
+
+    // Display timer on top of bomb
     let displayMins = Math.floor(remainingSeconds / 60);
     let displaySecs = remainingSeconds % 60;
     let timeStr = p.nf(displayMins, 2) + ':' + p.nf(displaySecs, 2);
-    p.fill(0);
+    p.fill("white");
+    p.textSize(32);
     p.text(timeStr, p.width / 2, p.height / 2);
   };
 
