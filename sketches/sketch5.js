cf18eba2399492a9bb634b960215ae182571a3fb
sowassup <sowassup@gmail.com>
2026-02-09 19:26:54 -0800

    add csv files for sketch5

--- sketches/sketch5.js
+++ sketches/sketch5.js
@@ -1,23 +1,12 @@
-// Example 2
+// Homework 5: Uber destination bar chart
 registerSketch('sk5', function (p) {
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    p.background("white");
   };
 
   p.draw = function () {
-    p.background(250);
-
-    // Corner time display
-    const h = p.hour();
-    const m = p.minute();
-    const s = p.second();
-    const label = p.nf(h, 2) + ':' + p.nf(m, 2) + ':' + p.nf(s, 2);
-
-    p.noStroke();
-    p.fill(20);
-    p.textAlign(p.LEFT, p.TOP);  // change to RIGHT/BOTTOM for other corners
-    p.textSize(50);
-    p.text(label, 12, 10);   // top-left corner
+    p.background("white");
 
   }
 
